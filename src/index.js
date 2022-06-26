78265ae61cd8856a4e8d57717f3cae9b7b429c4e
asya2320 <64478347+asya2320@users.noreply.github.com>
2022-06-26 15:06:45 +0300

    feat: upload code file

--- src/index.js
+++ src/index.js
@@ -38,9 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr_del_10=[];
+    let arr_str='';
+    let begin_slice=0;
+    let result='';
+    let length_1=Math.floor(expr.length/10);
+    let key = Object.keys(MORSE_TABLE);
+    for(let i=0; i<length_1; i++){
+        arr_del_10[i]=String(expr).slice(begin_slice, begin_slice+10);
+        begin_slice+=10;
+    }
+    for(let i=0; i<length_1; i++){
+        if(arr_del_10[i]=='**********'){
+            result+=' ';
+        }else{
+            for(let j=0; j<9; j=j+2){
+                let temp=arr_del_10[i][j]+arr_del_10[i][j+1];
+                if(temp=='10'){
+                    arr_str+=".";
+                }else if(temp=='11'){
+                    arr_str+="-";
+                }
+            }
+            for(let i=0; i<key.length; i++){
+                if (arr_str==key[i]){
+                    result+=MORSE_TABLE[key[i]];
+                }
+            }
+            arr_str='';
+        } 
+    }
+    let m=0;
+    
+    return result;
 }
 
 module.exports = {
     decode
+}
 }
